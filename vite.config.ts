8a7397e6e324ad4570f64c53ac9d5e8f15aa68c7
juck <juckz@foxmail.com>
2023-03-09 09:30:12 +0800

    chore: 1.loadEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { URL, fileURLToPath } from 'node:url';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import builtins from 'builtin-modules';
 
@@ -13,104 +13,109 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { ModuleFormat } from 'rollup';
 
-const prod = process.argv[3] !== '-w';
-const dir = process.env.OUTDIR ? process.env.OUTDIR : 'dest';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-    build: {
-        copyPublicDir: false,
-        outDir: dir,
-        lib: {
-            entry: [resolve(__dirname, 'src/main.ts')],
-            name: 'AwesomeBrainManager',
-            // the proper extensions will be added
-            fileName: (format: ModuleFormat, entryName: string) => {
-                if (format === 'umd') {
-                    return 'main.js';
-                } else {
-                    return entryName;
-                }
-            },
-            formats: ['umd'],
-        },
-        minify: prod ? true : false,
-        sourcemap: prod ? false : true,
-        rollupOptions: {
-            plugins: [
-                nodeResolve({
-                    // browser: true
-                }),
-                nodePolyfills({
-                    sourceMap: true,
-                }),
-            ],
-            output: {
-                exports: 'named',
-                assetFileNames: assetInfo => {
-                    if (assetInfo.name == 'style.css') {
-                        return 'styles.css';
+export default defineConfig(({ command, mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), 'VITE_');
+    const prod = process.argv[3] !== '-w';
+    const dir = process.env.OUTDIR ? process.env.OUTDIR : env['VITE_OUTDIR'] ? env['VITE_OUTDIR'] : 'dest';
+
+    return {
+        build: {
+            copyPublicDir: false,
+            outDir: dir,
+            lib: {
+                entry: [resolve(__dirname, 'src/main.ts')],
+                name: 'AwesomeBrainManager',
+                // the proper extensions will be added
+                fileName: (format: ModuleFormat, entryName: string) => {
+                    if (format === 'umd') {
+                        return 'main.js';
+                    } else {
+                        return entryName;
                     }
-                    return assetInfo.name || '';
                 },
-                // // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-                globals: {
-                    // vue: 'Vue',
-                },
+                formats: ['umd'],
             },
-            external: [
-                'obsidian',
-                'electron',
-                'moment',
-                '@codemirror/autocomplete',
-                '@codemirror/collab',
-                '@codemirror/commands',
-                '@codemirror/language',
-                '@codemirror/lint',
-                '@codemirror/search',
-                '@codemirror/state',
-                '@codemirror/view',
-                '@lezer/common',
-                '@lezer/highlight',
-                '@lezer/lr',
-                ...builtins,
+            minify: prod ? true : false,
+            sourcemap: prod ? false : true,
+            rollupOptions: {
+                plugins: [
+                    nodeResolve({
+                        // browser: true
+                    }),
+                    nodePolyfills({
+                        sourceMap: true,
+                    }),
+                ],
+                output: {
+                    exports: 'named',
+                    assetFileNames: assetInfo => {
+                        if (assetInfo.name == 'style.css') {
+                            return 'styles.css';
+                        }
+                        return assetInfo.name || '';
+                    },
+                    // // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                    globals: {
+                        // vue: 'Vue',
+                    },
+                },
+                external: [
+                    'obsidian',
+                    'electron',
+                    'moment',
+                    '@codemirror/autocomplete',
+                    '@codemirror/collab',
+                    '@codemirror/commands',
+                    '@codemirror/language',
+                    '@codemirror/lint',
+                    '@codemirror/search',
+                    '@codemirror/state',
+                    '@codemirror/view',
+                    '@lezer/common',
+                    '@lezer/highlight',
+                    '@lezer/lr',
+                    ...builtins,
+                ],
+            },
+        },
+        define: {
+            'process.env.NODE_ENV': '"production"',
+        },
+        plugins: [
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: '.hotreload',
+                        dest: '',
+                    },
+                ],
+            }),
+            svelte({
+                configFile: 'svelte.config.js',
+            }),
+            wasm(),
+            topLevelAwait(),
+            vue({
+                template: {
+                    compilerOptions: {
+                        isCustomElement: tag => ['webview'].includes(tag),
+                    },
+                },
+            }),
+            vueJsx(),
+        ],
+        optimizeDeps: {},
+        resolve: {
+            alias: [
+                {
+                    find: '@',
+                    replacement: fileURLToPath(new URL('./src', import.meta.url)),
+                },
             ],
         },
-    },
-    define: {
-        'process.env.NODE_ENV': '"production"',
-    },
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: '.hotreload',
-                    dest: '',
-                },
-            ],
-        }),
-        svelte({
-            configFile: 'svelte.config.js',
-        }),
-        wasm(),
-        topLevelAwait(),
-        vue({
-            template: {
-                compilerOptions: {
-                    isCustomElement: tag => ['webview'].includes(tag),
-                },
-            },
-        }),
-        vueJsx(),
-    ],
-    optimizeDeps: {},
-    resolve: {
-        alias: [
-            {
-                find: '@',
-                replacement: fileURLToPath(new URL('./src', import.meta.url)),
-            },
-        ],
-    },
-    logLevel: 'info',
+        logLevel: 'info',
+    };
 });
