91ecf469c3a8ded126e78c05fd547093700e4b6b
Juck <juckz@foxmail.com>
2023-04-25 17:58:26 +0800

    fix: 1.lang=ts

--- src/ui/DataViewTimeLine.vue
+++ src/ui/DataViewTimeLine.vue
@@ -5,21 +5,21 @@
         <!-- <Teleport to="body" disable="false">
             <div v-html="mdText"></div>
         </Teleport> -->
-        <div v-for="task in taskList" :key="task.blockId">{{ task.text }}</div>
+        <div v-for="task in taskList" :key="task.blockId" @mouseenter="previewTask(task)" @click="completeTask(task)">
+            {{ task.text }}
+        </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { Component, MarkdownPreviewView, MarkdownView } from 'obsidian';
 import { STask, getAPI as getDataviewApi } from 'obsidian-dataview';
-// import { type Ref, ref } from 'vue';
-import { ref } from 'vue';
+import { type Ref, ref } from 'vue';
 import { listTasks, queryAll, queryImportantTasks } from '@/utils/dataview';
 
 const keyword = ref('');
 let mdText = ref('');
-const taskList = ref([]);
-// const taskList = ref([]) as Ref<STask[]>;
+const taskList: Ref<STask[]> = ref([]);
 
 const searchHandle = async () => {
     const DataviewAPI = getDataviewApi();
@@ -35,6 +35,15 @@ const searchHandle = async () => {
     // executeJs
     // const res = await DataviewAPI.execute(queryImportantTasks, container, component, '');
     // console.log(res);
+};
+
+const previewTask = async (task: STask) => {
+    // xxx
+    console.log('previewTask', task);
+};
+
+const completeTask = async (task: STask) => {
+    console.log('completeTask', task);
 };
 </script>
 
