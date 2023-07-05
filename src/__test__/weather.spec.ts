410a50fe32e8b87fd25cae685641ec23e2d35196
Juck <juckz@foxmail.com>
2023-07-05 15:34:43 +0800

    feat: 1.修改单元测试

--- src/__test__/weather.spec.ts
+++ src/__test__/weather.spec.ts
@@ -1,17 +1,12 @@
 import { describe, expect, it } from 'vitest';
 import { getWeather } from '@/api/weather';
 
-const utils = {
-    getWeather: apiKey => {
-        return getWeather({
-            apiKey: apiKey || 'e6d27287b8d54b5da382f19086dac223',
-        });
-    },
-};
+const apiKey = 'e6d27287b8d54b5da382f19086dac223';
 describe('weather', () => {
-    it('getWeather', () => {
-        const weather = utils.getWeather(null);
+    it('getWeather', async () => {
+        const weather = await getWeather({ apiKey });
         console.log(weather);
         expect(weather).not.null;
+        expect(weather?.textDay).string;
     });
 });
