9c8611f9da79dd3394b31d14f9e99c0023e536a7
Juck <juckz@foxmail.com>
2023-09-15 15:56:10 +0800

    chore: 1.fix test engine

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'jsdom',
+        coverage: {
+            provider: 'v8'
+        },
     },
     resolve: {
         alias: {
