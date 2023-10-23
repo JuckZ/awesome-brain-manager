757270c1e99ad0530e74fe09d3df66dffbc73a52
Juck <juckz@foxmail.com>
2023-10-23 23:17:18 +0800

    docs: auto set lang

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -24,5 +24,34 @@ export default {
   enhanceApp({ app, router, siteData }: EnhanceAppContext) {
     app.component('MainFooter', MainFooter)
     app.component('AnnouncementBar', AnnouncementBar)
+    router.onBeforePageLoad = (to) => {
+      if (import.meta.env.DEV) return
+      fetch('https://api.gumengya.com/Api/UserInfo', {
+        method: "post",
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          format: 'json',
+        })
+      }).then(
+        response => response.json()
+      ).then(
+        res => {
+          if (res.data.code == 200) {
+            if (res.data.data.location.startsWith('中国') && !router.route.path.startsWith('/zh')) {
+              router.go(`/zh${router.route.path}`)
+              // zh-CN en-US
+              // siteData.value.lang = 'zh-CN'
+            }
+          } else {
+            console.log(res.data)
+          }
+        }
+      ).catch(
+        console.error
+      )
+    }
   }
 }
