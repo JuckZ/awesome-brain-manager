3d0830de7a22e5d1fd316678418fc5f472bbdc41
Juck <juckz@foxmail.com>
2024-12-16 23:16:32 +0800

    feat: update test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        globals: true,
         environment: 'jsdom',
         coverage: {
             provider: 'v8',
         },
+        include: ['src/__test__/**'],
+        exclude: ['**/tests-examples', 'node_modules', 'src/tests'],
     },
     resolve: {
         alias: {
